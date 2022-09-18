181145fba0c5c16ee9b9b0a7bf0ed80c386b42b7
sniclasj <sniclasj@gmail.com>
2022-09-18 15:22:28 +0000

    Correct typo in EMAIL_BACKEND code

--- main/settings.py
+++ main/settings.py
@@ -82,7 +82,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.email.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
