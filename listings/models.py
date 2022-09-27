05f75ee86010b3b8f5823e6f5ae9e5f5f1fc385c
sniclasj <sniclasj@gmail.com>
2022-09-27 15:48:41 +0000

    Rename Listings model Listing

--- listings/models.py
+++ listings/models.py
@@ -11,7 +11,7 @@ class Discipline(models.Model):
         return self.name
 
 
-class Listings(models.Model):
+class Listing(models.Model):
     lister = models.ForeignKey(
         'Athlete', null=False, blank=False, on_delete=models.CASCADE)
     discipline = models.ForeignKey(
