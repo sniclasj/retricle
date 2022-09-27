17a8b103e631dce38df6c78dfec8d538d2990a1e
sniclasj <sniclasj@gmail.com>
2022-09-27 15:38:29 +0000

    Add Athlete model to athletes app

--- athletes/models.py
+++ athletes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Athlete(models.Model):
+    athlete = models.OneToOneField('User', on_delete=models.CASCADE)
+    is_seller = models.BooleanField(
+        verbose_name='Is Seller?', null=False, default=False)
+
+    def __str__(self):
+        return self.athlete
